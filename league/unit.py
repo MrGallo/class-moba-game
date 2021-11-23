2e7862e24533bf23ca2689eef63ea7fb9943dfd9
Beva-n <brianwu312@gmail.com>
2021-11-23 10:44:24 -0500

    yes

--- league/unit.py
+++ league/unit.py
@@ -1,18 +1,12 @@
 import pygame
-# from team import Team
-# from ability import Ability
-# from stats import Stats
-
-class Team:
-    pass
-class Ability:
-    pass
-class Stats:
-    pass
+from team import Team
+from ability import Ability
+from stats import Stats
 
 class Unit:
     def __init__(self, base_health: int, base_damage: int, base_armor: int, base_range: int, base_speed: int, team: Team, enemy_team: Team) -> None:
         self._base_stats = Stats(base_health, base_damage, base_armor, base_range, base_speed)
+        self._current_stats = self._base_stats
         self._level = 1
         self._team = team
         self._enemy_team = enemy_team
@@ -29,24 +23,32 @@ class Unit:
         "Updates the player's damage, movement, armor, health, etc..."
         pass
 
+    def _update_stats(self, stats_added: Stats):
+        """updates the player's current stats base on the stats given by a item, buff, or debuff
+        
+        Args:
+            stats_added: stats provided by the item or some other sources
+        """
+        self._current_stats += stats_added
+
     def get_damage(self) -> int:
         "Gets the unit's damage"
-        return self._base_stats.damage
+        return self._current_stats.damage
 
     def get_health(self) -> int:
         "Gets the unit's health"
-        return self._base_stats.health
+        return self._current_stats.health
 
     def get_armor(self) -> int:
         "Gets the unit's armor value"
-        return self._base_stats.armor
+        return self._current_stats.armor
 
     def get_range(self) -> int:
         "Gets the unit's range"
-        return self._base_stats.range
+        return self._current_stats.range
 
     def get_speed(self) -> int:
-        return self._base_stats.speed
+        return self._current_stats.speed
     
     def get_level(self) -> int:
         return self._level
---
+++ tests/test_update_unit_stats.py
@@ -0,0 +1,24 @@
+from league.stats import Stats
+from league.unit import Unit
+
+def test_stat_getters():
+    unit1 = Unit(10, 5 ,14, 19, 35, 'Red', 'Blue')
+    """
+    health
+    damage
+    armor
+    range
+    speed
+    """
+    assert unit1.get_damage == 5
+    assert unit1.get_health == 10
+    assert unit1.get_armor == 14
+    assert unit1.get_range == 19
+    assert unit1.get_speed == 35
+
+def test_update_stats():
+    unit1 = Unit(10, 5 ,14, 19, 35, 'Red', 'Blue')
+    unit1._update_stats(Stats(health = 300, armor = 45, speed = 5))
+    assert unit1.get_health == 310
+    assert unit1.get_armor == 59
+    assert unit1.get_speed == 40
