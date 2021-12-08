6ac24613e9ad3b70bdb29c89fa15a9dc80622c72
mrgallo <daniel.gallo@ycdsbk12.ca>
2021-12-08 09:57:55 -0500

    Add title view and a view template

--- league/game.py
+++ league/game.py
@@ -3,4 +3,5 @@ from league.base_game import BaseGame
 
 class LeagueOfNoobsGame(BaseGame):
     def create(self) -> None:
-        pass
+        from league.title_view import TitleView
+        LeagueOfNoobsGame.set_current_view(TitleView())
